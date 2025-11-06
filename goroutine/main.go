3d00fb21b9d7857bf99b705f0e91a03e654b64b7
= <=>
2025-11-06 17:47:22 +0300

    goroutine

---
+++ goroutine/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func mine(n int) int {
+	fmt.Println("Поход в шахту номер", n, "начался...")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Поход в шахту номер", n, "закончился")
+	return 10
+}
+
+func main() {
+	coal := 0
+
+	before := time.Now()
+
+	coal += mine(1)
+	coal += mine(2)
+	coal += mine(3)
+
+	fmt.Println("Добыли", coal, "угля")
+	fmt.Println("Прошло времени", time.Since(before))
+}
