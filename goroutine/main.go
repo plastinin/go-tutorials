1d86191cd571dd3e75b7dc6718f93f9a09ec3137
plastinin <a.plastinin@gmail.com>
2025-11-07 18:30:23 +0300

    transferPoint

--- goroutine/main.go
+++ goroutine/main.go
@@ -5,21 +5,27 @@ import (
 	"time"
 )
 
-func mine(n int) int {
+func mine(transferPoint chan int, n int) {
 	fmt.Println("Поход в шахту номер", n, "начался...")
-	time.Sleep(10 * time.Second)
+	time.Sleep(1 * time.Second)
 	fmt.Println("Поход в шахту номер", n, "закончился")
-	return 10
+	transferPoint <- 10
 }
 
 func main() {
 	coal := 0
 
+	transferPoint := make(chan int)
+
 	before := time.Now()
 
-	coal += mine(1)
-	coal += mine(2)
-	coal += mine(3)
+	go mine(transferPoint, 1)
+	go mine(transferPoint, 2)
+	go mine(transferPoint, 3)
+
+	coal += <-transferPoint
+	coal += <-transferPoint
+	coal += <-transferPoint
 
 	fmt.Println("Добыли", coal, "угля")
 	fmt.Println("Прошло времени", time.Since(before))
