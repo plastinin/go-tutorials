6e12aa16be258f177fb6a701dbc4a5a035c25130
plastinin <a.plastinin@gmail.com>
2025-11-23 22:27:53 +0300

    headers and status added

--- http-wallet/main.go
+++ http-wallet/main.go
@@ -13,22 +13,47 @@ var bank = 0
 var mtx = sync.Mutex{}
 
 func payHandler(w http.ResponseWriter, r *http.Request) {
+	
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	} else {
+		fmt.Println("Method: ", r.Method)
+	}
+
 	httpRequestBody, err := io.ReadAll(r.Body)
 	if err != nil {
-		fmt.Println("Read IO error:", err)
+		msg := "Read IO error:" + err.Error()
+		fmt.Println(msg)
+		w.WriteHeader(http.StatusInternalServerError)
+		_, err := w.Write([]byte(msg))
+		if err != nil {
+			fmt.Println("failed to write HTTP-response:", err)
+		}
 		return
 	}
 
 	httpRequestBodyString := string(httpRequestBody)
 	paymentAmount, err := strconv.Atoi(httpRequestBodyString)
 	if err != nil {
-		fmt.Println("Failed to convert", err)
+		msg := "Failed to convert:" + err.Error()
+		fmt.Println(msg)
+		w.WriteHeader(http.StatusBadRequest)
+		_, err := w.Write([]byte(msg))
+		if err != nil {
+			fmt.Println("failed to write HTTP-response:", err)
+		}
 		return
 	}
 	mtx.Lock()
 	if money - paymentAmount >= 0 {
 		money -= paymentAmount
-		fmt.Println("Pay success:", paymentAmount, "USD. Balance:", money)
+		msg := "Pay success: " + strconv.Itoa(paymentAmount) + " USD. Balance: " + strconv.Itoa(money)
+		fmt.Println(msg)
+		_, err := w.Write([]byte(msg))
+		if err != nil {
+			fmt.Println("failed to write HTTP-response:", err)
+		}
 	}
 	mtx.Unlock()
 }
@@ -36,7 +61,12 @@ func payHandler(w http.ResponseWriter, r *http.Request) {
 func saveHandler(w http.ResponseWriter, r *http.Request) {
 	httpRequestBody, err := io.ReadAll(r.Body)
 	if err != nil {
-		fmt.Println("Read IO error:", err)
+		msg := "Read IO error:" + err.Error()
+		fmt.Println(msg)
+		_, err := w.Write([]byte(msg))
+		if err != nil {
+			fmt.Println("failed to write HTTP-response:", err)
+		}
 		return
 	}
 
