622c734e6fabb9ef451e142ac8dae66666377a7c
plastinin <a.plastinin@gmail.com>
2025-11-24 18:54:33 +0300

    jsom

---
+++ json/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+type Payment struct {
+	Description string `json:"description"` // описание покупки
+	USD         int    `json:"USD"`         // сумма покупк
+	FullName    string `json:"fullName"`    // ФИО
+	Address     string `json:"address"`     // Адрес
+}
+
+func (p Payment) Println() {
+	fmt.Println("description:", p.Description)
+	fmt.Println("USD:", p.USD)
+	fmt.Println("fullName:", p.FullName)
+	fmt.Println("address:", p.Address)
+}
+
+var money = 1000
+var paymentHistory = make([]Payment, 0)
+var mtx = sync.Mutex{}
+
+func payHandler(w http.ResponseWriter, r *http.Request) {
+
+	var payment Payment
+	if err := json.NewDecoder(r.Body).Decode(&payment); err != nil {
+		fmt.Println("err:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	mtx.Lock()
+	payment.Println()
+
+	if money-payment.USD >= 0 {
+		money -= payment.USD
+	}
+	paymentHistory = append(paymentHistory, payment)
+
+	fmt.Println("money:", money)
+	fmt.Println("history:", paymentHistory)
+	mtx.Unlock()
+
+}
+
+func main() {
+	http.HandleFunc("/pay", payHandler)
+	fmt.Println("Starting http-service...")
+	if err := http.ListenAndServe(":9091", nil); err != nil {
+		fmt.Println("Error connect:", err.Error())
+	}
+}
